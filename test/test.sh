de0c7d77b6b4d5d942a5548f6bb0c88388105c66
alextidd <at31@sanger.ac.uk>
2024-04-19 00:53:47 +0100

    add singularity module to nextflow runs

--- test/test.sh
+++ test/test.sh
@@ -1,14 +1,19 @@
 #!/bin/bash
-# cd /lustre/scratch126/casm/team154pc/at31/chemo_trees/nf-chemo-trees/test/; mamba activate trees ; ~/bin/jsub lsf -q week -n test_chemo_trees  -m 2g -l log "bash test.sh" | bsub
+# cd /lustre/scratch126/casm/team154pc/at31/chemo_trees/nf-chemo-trees/test/; mamba activate trees ; ~/bin/jsub lsf -q week -n test_chemo_trees -m 2g -l log "bash test.sh" | bsub
 
+# dirs
 wd=/lustre/scratch126/casm/team154pc/at31/chemo_trees/nf-chemo-trees/test/
 cd $wd
+
+# load singularity
+module load singularity
 
 # run the workflow on the hg38 test data
 nextflow run ../main.nf \
     --sample_sheet data/GRCh38/sample_sheet.csv \
     --outdir out/GRCh38/ \
     -w work/ \
-    -c /lustre/scratch125/casm/team268im/at31/RA_som_mut/scomatic/config/LSF.config
+    -c /lustre/scratch125/casm/team268im/at31/RA_som_mut/scomatic/config/LSF.config \
+    -resume
 
 # run the workflow on hg37 test data
