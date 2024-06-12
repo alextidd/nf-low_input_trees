08dc2b8a53974e46ccb1c32ba64bd360d886a3a4
alextidd <at31@sanger.ac.uk>
2024-06-12 15:12:59 +0100

    add test script

--- test/setup.sh
+++ test/setup.sh
@@ -1,48 +1,11 @@
 #!/bin/bash
-wd=/lustre/scratch126/casm/team154pc/at31/low_input_sanger_filtering/test/
-cd $wd
 
-# modules
-module load samtools-1.19.2/python-3.11.6
+# dirs
+bd=/lustre/scratch126/casm/team154pc/at31/
+cd $bd/low_input_sanger_filtering/
 
-# generate GRCh38 test datasets (chr22 is small, chr10 is medium)
-for chr in chr22 chrX ; do
-    echo $chr
-    outdir=$wd/data/GRCh38/$chr/
-    mkdir -p $outdir
-
-    head -1 ../../data/sample_sheet.csv > $outdir/sample_sheet.csv
-    while IFS=, read -r donor_id sample_id project_id experiment_id bam pindel_vcf caveman_vcf ; do
-
-        # subset vcfs to chr
-        new_vcfs=()
-        for vcf in $pindel_vcf $caveman_vcf; do
-            base_vcf=$(basename $vcf)
-            new_vcf="$outdir/${base_vcf}"
-            zcat $vcf | grep "^#\|^$chr" | gzip \
-            > $new_vcf
-            new_vcfs+=($new_vcf)
-        done
-
-        # subset bam to chr
-        base_bam=$(basename $bam)
-        new_bam="$outdir/${base_bam}"
-        samtools view -bo $new_bam $bam $chr
-
-        # create bai, bas and met.gz files
-        samtools index $new_bam
-        samtools idxstats $new_bam > $new_bam.bas
-        samtools flagstat $new_bam | gzip > $new_bam.met.gz
-
-        # add to sample sheet
-        echo "$donor_id,$sample_id,$project_id,$experiment_id,$new_bam,${new_vcfs[0]},${new_vcfs[1]}" \
-        >> $outdir/sample_sheet.csv
-
-    done < <(cat ../../data/sample_sheet.csv | sed 1d | head -2)
-done
-
-# test dataset for two full samples
-mkdir -p data/GRCh38/full/
-cat ../../data/sample_sheet.csv |
-head -5 \
-> data/GRCh38/full/sample_sheet.csv 
+# test dataset for 3 full samples
+mkdir -p test/data/GRCh38/
+cat $bd/chemo_trees/out/metadata/sample_sheet.csv |
+head -4 \
+> test/data/GRCh38/sample_sheet.csv 
--- test/test.sh
+++ test/test.sh
@@ -1,23 +1,15 @@
 #!/bin/bash
-# cd /lustre/scratch126/casm/team154pc/at31/chemo_trees/nf-chemo-trees/test/; bsub -q week -M 2000 -R 'select[mem>=2000] span[hosts=1] rusage[mem=2000]' -J test_chemo_trees -o log "bash test.sh"
+# cd /lustre/scratch126/casm/team154pc/at31/low_input_sanger_filtering/test/; bsub -q week -M 2000 -R 'select[mem>=2000] span[hosts=1] rusage[mem=2000]' -J test_lisf -o log "bash test.sh"
 
 # modules
 module load singularity
 
-# # run the workflow on the hg38 test data, chr22
-# nextflow run ../main.nf \
-#    --sample_sheet data/GRCh38/chr22/sample_sheet.csv \
-#    --outdir out/GRCh38/chr22/ \
-#    -w work/ \
-#    -c /lustre/scratch125/casm/team268im/at31/RA_som_mut/scomatic/config/LSF.config \
-#    -resume
-
-# run the workflow on the hg38 test data, two full samples
+# run the workflow on the hg38 test data, 3 full samples
 nextflow run ../main.nf \
-    --sample_sheet data/GRCh38/full/sample_sheet.csv \
+    --sample_sheet data/GRCh38/sample_sheet.csv \
     --project_type WES \
-    --outdir out/GRCh38/full/ \
+    --outdir out/GRCh38/ \
     -w ./work/ \
-    -c /lustre/scratch125/casm/team268im/at31/RA_som_mut/scomatic/config/LSF.config \
+    -c ../conf/LSF.config \
     -resume \
     -N at31@sanger.ac.uk
