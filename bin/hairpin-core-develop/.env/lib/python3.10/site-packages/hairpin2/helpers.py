61baeaa9a0de7f2fe515fb673b88dd4bbac2c8b5
alextidd <at31@sanger.ac.uk>
2024-10-17 10:11:37 +0100

    add hairpin2 binary

---
+++ bin/hairpin-core-develop/.env/lib/python3.10/site-packages/hairpin2/helpers.py
@@ -0,0 +1,48 @@
+from enum import IntEnum, Flag
+import logging
+import sys
+from hairpin2 import constants as c
+
+
+def cleanup(code: int = c.EXIT_FAILURE, msg: None | str = None) -> None:
+    if code != c.EXIT_SUCCESS and msg:
+        logging.error(msg)
+    if code == c.EXIT_SUCCESS:
+        logging.info('hairpin complete')
+    sys.exit(code)
+
+
+def test_options(args):
+    if not (0 < args.min_clip_quality < 93):
+        cleanup(msg='invalid --min-clip-quality; range 0-93')
+    if not (0 < args.min_mapping_quality < 60):
+        cleanup(msg='invalid --min-mapping-quality; range 0-60')
+    if not (0 < args.min_base_quality < 93):
+        cleanup(msg='invalid --min-base-quality; range 0-93')
+    if not (0 < args.position_fraction < 1):
+        cleanup(msg='invalid --position-fraction; range 0-1')
+
+
+def has_duplicates(
+    l: list
+) -> bool:
+    return len(l) != len(set(l))
+
+
+def lists_not_equal(
+    l1: list | set,
+    l2: list | set
+) -> bool:
+    return sorted(l1) != sorted(l2)
+
+
+def print_flag(
+    print_enum: Flag
+) -> None:
+    print([':'.join([str(e), hex(e.value)]) for e in print_enum])
+
+
+def print_enum(
+    print_enum: IntEnum
+) -> None:
+    print([e for e in print_enum])
